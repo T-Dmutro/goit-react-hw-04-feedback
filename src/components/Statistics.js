0088b450263b1f3e4595427a7afc01e665f652aa
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-05-08 13:39:58 +0300

    finish

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -7,8 +7,8 @@ export const Statistics = ({ onGood, onNeutral, onBad, onCountTotalFeedback,onCo
       <TextP>Good: <Number>{onGood}</Number></TextP>
       <TextP>Neutral: <Number>{onNeutral}</Number></TextP>
       <TextP>Bad:  <Number>{onBad}</Number></TextP>
-      <TextP>Total: <Number>{onCountTotalFeedback()}</Number></TextP>
-      <TextP>Positive feedback: <Number>{onCountPositiveFeedbackPercentage()}</Number></TextP>
+      <TextP>Total: <Number>{onCountTotalFeedback}</Number></TextP>
+      <TextP>Positive feedback: <Number>{onCountPositiveFeedbackPercentage}</Number></TextP>
     </Section>
   );
 };
--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -1,90 +1,146 @@
 import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from './FeedbackOptions';
 import { Statistics } from './Statistics.js';
 import {Section, Title} from './Section.styled'
-class Counter extends React.Component {
-  static defaultProps = {
-    initState: {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    },
-  };
-  state = {
-    good: this.props.initState.good,
-    neutral: this.props.initState.neutral,
-    bad: this.props.initState.bad,
-  };
-  btnReview = event => {
-    const btnValue = event.currentTarget.value;
-    this.setState(prevState => ({
-      ...prevState,
-      [btnValue]: prevState[btnValue] + 1,
-    }));
-  };
-  // handButtonPressGood = () => {
-  //   console.log(`добре ${this.state.good}`);
-  //   this.setState(prevState => ({
-  //     good: prevState.good + 1,
-  //   }));
-  // };
-  // handButtonPressNeutral = () => {
-  //   console.log(`нейтрально ${this.state.neutral}`);
-  //   this.setState(prevState => ({
-  //     neutral: prevState.neutral + 1,
-  //   }));
-  // };
-  // handButtonPressBad = () => {
-  //   console.log(`погано ${this.state.bad}`);
-  //   this.setState(prevState => ({
-  //     bad: prevState.bad + 1,
-  //   }));
-  // };
-  countTotalFeedback = () => {
-    let total = this.state.good + this.state.neutral + this.state.bad;
-    
-    return total;
-  };
-  countPositiveFeedbackPercentage = () => {
-    let percentage = `${Math.round(
-      (this.state.good /
-        (this.state.good + this.state.neutral + this.state.bad)) *
-        100
-    )}%`;
-    return percentage;
-  };
+// class Counter extends React.Component {
+//   static defaultProps = {
+//     initState: {
+//       good: 0,
+//       neutral: 0,
+//       bad: 0,
+//     },
+//   };
+//   state = {
+//     good: this.props.initState.good,
+//     neutral: this.props.initState.neutral,
+//     bad: this.props.initState.bad,
+//   };
+//   btnReview = event => {
+//     const btnValue = event.currentTarget.value;
+//     this.setState(prevState => ({
+//       ...prevState,
+//       [btnValue]: prevState[btnValue] + 1,
+//     }));
+//   };
+//   // handButtonPressGood = () => {
+//   //   console.log(`добре ${this.state.good}`);
+//   //   this.setState(prevState => ({
+//   //     good: prevState.good + 1,
+//   //   }));
+//   // };
+//   // handButtonPressNeutral = () => {
+//   //   console.log(`нейтрально ${this.state.neutral}`);
+//   //   this.setState(prevState => ({
+//   //     neutral: prevState.neutral + 1,
+//   //   }));
+//   // };
+//   // handButtonPressBad = () => {
+//   //   console.log(`погано ${this.state.bad}`);
+//   //   this.setState(prevState => ({
+//   //     bad: prevState.bad + 1,
+//   //   }));
+//   // };
+//   countTotalFeedback = () => {
+//     let total = this.state.good + this.state.neutral + this.state.bad;
 
-  render() {
-    // const { good, neutral, bad } = this.state;
-    const btnNames = Object.keys(this.state);
-    return (
-      <div>
-        <Section><Title>Please leave feedback</Title></Section>
-          <Button
-          options={btnNames} onReview={this.btnReview} />
-             {/* onButtonPressGood={this.handButtonPressGood}
-             onButtonPressNeutral={this.handButtonPressNeutral}
-             onButtonPressBad={this.handButtonPressBad} */}
-         
-          
-          {this.countTotalFeedback() ? (
-            <Statistics
-              onGood={this.state.good}
-              onNeutral={this.state.neutral}
-              onBad={this.state.bad}
-              onCountTotalFeedback={this.countTotalFeedback}
-              onCountPositiveFeedbackPercentage={
-                this.countPositiveFeedbackPercentage
-              }
-            />
-          ) : null}
-       
-        {!this.countTotalFeedback() ? <Section><Title>There is no feedback</Title></Section> : null}
-      </div>
-    );
-  }
+//     return total;
+//   };
+//   countPositiveFeedbackPercentage = () => {
+//     let percentage = `${Math.round(
+//       (this.state.good /
+//         (this.state.good + this.state.neutral + this.state.bad)) *
+//         100
+//     )}%`;
+//     return percentage;
+//   };
+
+//   render() {
+//     // const { good, neutral, bad } = this.state;
+//     const btnNames = Object.keys(this.state);
+//     return (
+//       <div>
+//         <Section><Title>Please leave feedback</Title></Section>
+//           <Button
+//           options={btnNames} onReview={this.btnReview} />
+//              {/* onButtonPressGood={this.handButtonPressGood}
+//              onButtonPressNeutral={this.handButtonPressNeutral}
+//              onButtonPressBad={this.handButtonPressBad} */}
+
+
+//           {this.countTotalFeedback() ? (
+//             <Statistics
+//               onGood={this.state.good}
+//               onNeutral={this.state.neutral}
+//               onBad={this.state.bad}
+//               onCountTotalFeedback={this.countTotalFeedback}
+//               onCountPositiveFeedbackPercentage={
+//                 this.countPositiveFeedbackPercentage
+//               }
+//             />
+//           ) : null}
+
+//         {!this.countTotalFeedback() ? <Section><Title>There is no feedback</Title></Section> : null}
+//       </div>
+//     );
+//   }
+// }
+
+
+
+
+const Counter =()=>{
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+const  btnReview = event => {
+    const btnName = event.currentTarget.value;
+    switch (btnName) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        return;
+  };
 }
+
+const countTotalFeedback = good + neutral + bad;
+const countPositiveFeedbackPercentage = `${Math.round(good / (good + neutral + bad) * 100)}`
+return(
+  <div>
+    <Section><Title>Please leave feedback</Title></Section>
+      <Button
+      options={['good', 'neutral', 'bad']} onReview={btnReview} />
+         {/* onButtonPressGood={this.handButtonPressGood}
+         onButtonPressNeutral={this.handButtonPressNeutral}
+         onButtonPressBad={this.handButtonPressBad} */}
+
+
+      {countTotalFeedback ? (
+        <Statistics
+          onGood={good}
+          onNeutral={neutral}
+          onBad={bad}
+          onCountTotalFeedback={countTotalFeedback}
+          onCountPositiveFeedbackPercentage={
+            countPositiveFeedbackPercentage
+          }
+        />
+      ) : null}
+
+    {!countTotalFeedback ? <Section><Title>There is no feedback</Title></Section> : null}
+  </div>
+);
+        }
 Button.propTypes={
   onButtonPressGood:PropTypes.func,
   onButtonPressNeutral:PropTypes.func,
@@ -95,12 +151,15 @@ Statistics.propTypes={
   onGood:PropTypes.number,
   onNeutral:PropTypes.number,
   onBad:PropTypes.number,
-  onCountTotalFeedback:PropTypes.func,
-  onCountPositiveFeedbackPercentage:PropTypes.func,
+  onCountTotalFeedback:PropTypes.number,
+  onCountPositiveFeedbackPercentage:PropTypes.string,
 
 }
 
+
 export default Counter;
+
+
 
 // <button type="button" onClick={this.handButtonPressGood}>
 // Good
